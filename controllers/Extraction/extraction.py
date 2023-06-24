0d489c5e83b48c9717ab3fe80a2cc4437a8d023b
Avirup Sett <settavirup@gmail.com>
2023-06-24 21:23:52 +0530

    Added /api/v1/find_route

--- controllers/Extraction/extraction.py
+++ controllers/Extraction/extraction.py
@@ -46,3 +46,53 @@ def Extracting(Product_Type):
     df.reset_index(inplace=True,drop=True)
 
     return df
+
+def ExtractingFromOfficeId(Product_Type,OfficeList):
+
+    response = requests.get(url)
+    data = response.json()
+
+    # convert the JSON data to a Pandas DataFrame
+    df = pd.DataFrame(data,columns=['productTypeId','officeName',"latitude","longitude","avgSales","officeId"])
+   
+
+    godown_df = pd.DataFrame(data)
+
+    # Define a function that takes a row of the dataframe as input and returns a tuple of total_capacity and total_current_stock
+    def calculate_total_capacity_and_stock(row):
+
+        if len(row["godownProducts"]):
+            total_capacity = sum(list(pd.DataFrame(row["godownProducts"])["capacity"]))
+            total_current_stock = sum(list(pd.DataFrame(row["godownProducts"])["currentStock"]))
+        else:
+            total_capacity=0
+            total_current_stock=0
+
+        return total_capacity, total_current_stock
+
+    # Use apply() method to apply the function on each row of the dataframe and store the results in new columns
+    godown_df[["totalCapacity", "totalCurrentStock"]] = godown_df.apply(calculate_total_capacity_and_stock, axis=1, result_type="expand")
+
+    # Extract the columns into separate lists
+    currentStock = godown_df["totalCurrentStock"].tolist()
+    capacity = godown_df["totalCapacity"].tolist()
+    
+    df = df.assign(currentStock=currentStock,totalCapacity=capacity)
+     # Select the rows where officeId is in the OfficeList
+    df=df[df["officeId"].isin(OfficeList)]
+    df.reset_index(inplace=True,drop=True)
+ 
+    df["requirement%"]=(abs(df["totalCapacity"]-df["currentStock"])/df["totalCapacity"])*100
+
+    df=df[df["productTypeId"]==Product_Type]
+    df.reset_index(inplace=True,drop=True)
+    for i in range(len(df)):
+        df.loc[i,"requirement%"]=100 if df.loc[i,"requirement%"]>100 else df.loc[i,"requirement%"]
+
+    df["requirement%"].fillna(0,inplace=True)
+    df.dropna(inplace=True)
+    df.reset_index(inplace=True,drop=True)
+    df.sort_values(by="requirement%",inplace=True,ascending=False)
+    df.reset_index(inplace=True,drop=True)
+
+    return df
--- routes/route.py
+++ routes/route.py
@@ -1,12 +1,13 @@
 from flask import jsonify, request
 import requests
 import pandas as pd
-from controllers.Extraction.extraction import Extracting
+from controllers.Extraction.extraction import Extracting, ExtractingFromOfficeId
 from controllers.DistanceAway.distanceaway import DistanceAwayFromStartingPoint
 from controllers.Filteration.filtration import Filtering
 from controllers.RouteFinding.Algo01 import Route_plan_without_priority
-from controllers.RouteFinding.Algo02 import Route_plan_with_priority_V1
-from controllers.RouteFinding.Algo03 import Route_plan_with_priority_V2
+
+# from controllers.RouteFinding.Algo02 import Route_plan_with_priority_V1
+# from controllers.RouteFinding.Algo03 import Route_plan_with_priority_V2
 from flask import Blueprint
 from config.config import ManufacturingHuburl
 
@@ -23,17 +24,19 @@ def create_post():
     response = requests.get(ManufacturingHuburl)
     data = response.json()
 
-
     # convert the JSON data to a Pandas DataFrame
     Starting_Point_df = pd.DataFrame(data)
     Starting_PointName = Starting_Point_df.loc[
         Starting_Point_df["cityId"] == Starting_PointId, "cityName"
     ].values[0]
-    
-    Starting_Point_latitude =Starting_Point_df.loc[Starting_Point_df["cityId"] == Starting_PointId, "latitude"].values[0]
-    
-    Starting_Point_longitude = Starting_Point_df.loc[Starting_Point_df["cityId"] == Starting_PointId, "longitude"].values[0]
-    
+
+    Starting_Point_latitude = Starting_Point_df.loc[
+        Starting_Point_df["cityId"] == Starting_PointId, "latitude"
+    ].values[0]
+
+    Starting_Point_longitude = Starting_Point_df.loc[
+        Starting_Point_df["cityId"] == Starting_PointId, "longitude"
+    ].values[0]
 
     df = Extracting(Product_TypeId)
     df = DistanceAwayFromStartingPoint(
@@ -44,7 +47,11 @@ def create_post():
     )
 
     optimal_route1 = Route_plan_without_priority(
-        df, Starting_PointName,str(Starting_PointId), Starting_Point_latitude, Starting_Point_longitude
+        df,
+        Starting_PointName,
+        str(Starting_PointId),
+        Starting_Point_latitude,
+        Starting_Point_longitude,
     )
     # optimal_route2 = Route_plan_with_priority_V1(
     #     df, Starting_PointName,str(Starting_PointId), Starting_Point_latitude, Starting_Point_longitude
@@ -73,5 +80,60 @@ def create_post():
             #     "Route": optimal_route3[0],
             #     "Total_distance": optimal_route3[1],
             # },
+        },
+    )
+
+
+@route_page.route("/api/v1/find_route", methods=["POST"])
+def find_route():
+    Product_TypeId = request.json["ProductTypeId"]
+    Starting_PointId = request.json["StartingPointId"]
+    Tank_Capacity = request.json["TankCapacity"]
+    minimum_multiple = request.json["MinimumMultiple"]
+    No_of_days_for_delivery = request.json["No_of_days_for_delivery"]
+    Office_list = request.json["OfficeIdList"]
+
+    response = requests.get(ManufacturingHuburl)
+    data = response.json()
+
+    # convert the JSON data to a Pandas DataFrame
+    Starting_Point_df = pd.DataFrame(data)
+    Starting_PointName = Starting_Point_df.loc[
+        Starting_Point_df["cityId"] == Starting_PointId, "cityName"
+    ].values[0]
+
+    Starting_Point_latitude = Starting_Point_df.loc[
+        Starting_Point_df["cityId"] == Starting_PointId, "latitude"
+    ].values[0]
+
+    Starting_Point_longitude = Starting_Point_df.loc[
+        Starting_Point_df["cityId"] == Starting_PointId, "longitude"
+    ].values[0]
+
+    df = ExtractingFromOfficeId(Product_TypeId, Office_list)
+    df = DistanceAwayFromStartingPoint(
+        df, Starting_Point_latitude, Starting_Point_longitude
+    )
+    df, total_requirement, excess_capacity, Not_selected = Filtering(
+        df, Tank_Capacity, No_of_days_for_delivery, minimum_multiple
+    )
+
+    optimal_route1 = Route_plan_without_priority(
+        df,
+        Starting_PointName,
+        str(Starting_PointId),
+        Starting_Point_latitude,
+        Starting_Point_longitude,
+    )
+    return jsonify(
+        Total_requirement=total_requirement,
+        Excess_capacity=excess_capacity,
+        Not_selected=Not_selected,
+        Routes={
+            "Algorithm_1": {
+                "Description": "Routing based on Nearest Branch",
+                "Route": optimal_route1[0],
+                "Total_distance": optimal_route1[1],
+            },
         },
     )
